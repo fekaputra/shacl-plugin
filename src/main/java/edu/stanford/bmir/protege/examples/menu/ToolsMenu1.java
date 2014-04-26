3efb0b132662e2eb1149ae680e1750b11cffede4
Jennifer Vendetti <vendetti@stanford.edu>
2014-04-25 18:56:32 -0700

    Fixed warning and changed message text.

--- src/main/java/edu/stanford/bmir/protege/examples/menu/ToolsMenu1.java
+++ src/main/java/edu/stanford/bmir/protege/examples/menu/ToolsMenu1.java
@@ -4,29 +4,22 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.JOptionPane;
 
-import org.apache.log4j.Logger;
-
 import org.protege.editor.owl.ui.action.ProtegeOWLAction;
 
 public class ToolsMenu1 extends ProtegeOWLAction {
 	private static final long serialVersionUID = -2896209622461162777L;
 
 	public void initialise() throws Exception {
-
 	}
-
 
 	public void dispose() throws Exception {
-
 	}
 
-
 	public void actionPerformed(ActionEvent event) {
-		StringBuffer message = new StringBuffer("This menu is under the tools menu which is a standard part of Protege\n");
+		StringBuffer message = new StringBuffer("This example menu item is under the Tools menu.\n");
 		message.append("The currently selected class is ");
 		message.append(getOWLWorkspace().getOWLSelectionModel().getLastSelectedClass());
 		message.append(".");
 		JOptionPane.showMessageDialog(getOWLWorkspace(), message.toString());	
 	}
-
 }
