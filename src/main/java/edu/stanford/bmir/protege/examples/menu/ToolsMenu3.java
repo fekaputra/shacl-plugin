d869942674c05d03099dc8b696f073ca0d181067
Jennifer Vendetti <vendetti@stanford.edu>
2014-04-25 18:41:01 -0700

    Fixed warning and cleaned up formatting.

--- src/main/java/edu/stanford/bmir/protege/examples/menu/ToolsMenu3.java
+++ src/main/java/edu/stanford/bmir/protege/examples/menu/ToolsMenu3.java
@@ -1,26 +1,19 @@
 package edu.stanford.bmir.protege.examples.menu;
 
 import java.awt.event.ActionEvent;
-
 import javax.swing.JOptionPane;
-
 import org.apache.log4j.Logger;
-
 import org.protege.editor.owl.ui.action.ProtegeOWLAction;
 
 public class ToolsMenu3 extends ProtegeOWLAction {
+	private static final long serialVersionUID = 1439595221948942812L;
 	public static Logger LOGGER = Logger.getLogger(ToolsMenu3.class);
 
-
 	public void initialise() throws Exception {
-
 	}
-
 
 	public void dispose() throws Exception {
-
 	}
-
 
 	public void actionPerformed(ActionEvent event) {
 		StringBuffer message = new StringBuffer("This menu is under the tools menu but is in a separate category from the above two.\n");
@@ -29,5 +22,4 @@ public class ToolsMenu3 extends ProtegeOWLAction {
 		message.append(" classes.");
 		JOptionPane.showMessageDialog(getOWLWorkspace(), message.toString());	
 	}
-
 }
