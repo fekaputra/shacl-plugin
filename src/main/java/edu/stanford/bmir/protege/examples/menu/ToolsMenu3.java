20941bfc8840c0c11543b4d81f5216538a66dd1f
Jennifer Vendetti <vendetti@stanford.edu>
2014-04-25 18:55:47 -0700

    Changed message text and cleaned up formatting.

--- src/main/java/edu/stanford/bmir/protege/examples/menu/ToolsMenu3.java
+++ src/main/java/edu/stanford/bmir/protege/examples/menu/ToolsMenu3.java
@@ -1,8 +1,11 @@
 package edu.stanford.bmir.protege.examples.menu;
 
 import java.awt.event.ActionEvent;
+
 import javax.swing.JOptionPane;
+
 import org.apache.log4j.Logger;
+
 import org.protege.editor.owl.ui.action.ProtegeOWLAction;
 
 public class ToolsMenu3 extends ProtegeOWLAction {
@@ -16,7 +19,8 @@ public class ToolsMenu3 extends ProtegeOWLAction {
 	}
 
 	public void actionPerformed(ActionEvent event) {
-		StringBuffer message = new StringBuffer("This menu is under the tools menu but is in a separate category from the above two.\n");
+		StringBuffer message = new StringBuffer(
+				"This example menu item is under the Tools menu, but displayed in a separate category from the above two.\n");
 		message.append("The active ontology has ");
 		message.append(getOWLModelManager().getActiveOntology().getClassesInSignature().size());
 		message.append(" classes.");
